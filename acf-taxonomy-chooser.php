defe6c4f2d71941dd7ba8081dd3959a393ae521b
Controlled Chaos Design <greg@ccdzine.com>
2019-12-25 22:49:15 -0800

    Plugin header, tidy code.

--- acf-taxonomy-chooser.php
+++ acf-taxonomy-chooser.php
@@ -1,35 +1,21 @@
 <?php
-
 /*
- * Plugin Name: Advanced Custom Fields: Taxonomy Chooser
- * Plugin URI: https://github.com/reyhoun/acf-taxonomy-chooser
- * Description: Choose from any taxonomy or term
+ * Plugin Name: ACF Taxonomy Chooser
+ * Plugin URI: https://github.com/ControlledChaos/acf-term-and-taxonomy-chooser
+ * Description: Choose from any taxonomy or term.
  * Version: 1.2
- * Author: Marktime Media
- * Author URI: http://marktimemedia.com
+ * Author: Marktime Media, Controlled Chaos Design
+ * Author URI:
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * GitHub Plugin URI: https://github.com/reyhoun/acf-taxonomy-chooser
+ * GitHub Plugin URI: https://github.com/ControlledChaos/acf-term-and-taxonomy-chooser
 */
 
+// Set text domain.
+load_plugin_textdomain( 'acf-taxonomy-chooser', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
-
-
-// 1. set text domain
-// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-taxonomy-chooser', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
-
-
-
-
-// 2. Include field type for ACF5
-// $version = 5 and can be ignored until ACF6 exists
+// Include field type for ACF5.
 function include_field_types_taxonomy_chooser( $version ) {
-	
-	include_once('acf-taxonomy-chooser-v5.php');
-	
+	include_once( 'acf-taxonomy-chooser-v5.php' );
 }
-
-add_action('acf/include_field_types', 'include_field_types_taxonomy_chooser');	
-
-?>
+add_action( 'acf/include_field_types', 'include_field_types_taxonomy_chooser' );
